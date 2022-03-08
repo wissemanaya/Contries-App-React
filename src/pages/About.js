16088f963e70fa967d8ba03ace5891905cf90c89
wissem <wissemanaya1@gmail.com>
2022-03-08 19:47:11 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+const About = () => {
+    return(
+        <div>
+            <h1> About </h1>
+            <p>ssdfsdfsdffnjefbioefbzeefzefzefezfzefzefzefeefe
+                ejjfbzefbzuiebfuiefbuiefbzefefzefzefefzezfeffe
+                eonfzefuibefizebfiebfzeefzefeffzefzefzefefzef
+                efnoefnoenfoefneofneoifnezoifneoifnzeofinzfefe
+                pfpozejfpejfpoefjpoefjpoefjpzoejfpefjzfzefezfe
+                efnioeznfoienfenfoiefnioefnoiefnoeifneoifnefef
+                efneiofneofneoifneoifnezoifnzeoifneofnioefneiofn
+            </p>
+            <p>ssdfsdfsdffnjefbioefbzeefzefzefezfzefzefzefeefe
+                ejjfbzefbzuiebfuiefbuiefbzefefzefzefefzezfeffe
+                eonfzefuibefizebfiebfzeefzefeffzefzefzefefzef
+                efnoefnoenfoefneofneoifnezoifneoifnzeofinzfefe
+                pfpozejfpejfpoefjpoefjpoefjpzoejfpefjzfzefezfe
+                efnioeznfoienfenfoiefnioefnoiefnoeifneoifnefef
+                efneiofneofneoifneoifnezoifnzeoifneofnioefneiofn
+                efezfezfezfzefzefzefezfefezfezfsdfghthththrtrtyjy
+                thrthrthrthrthrthrthrthrtyr(-utyjkuyukyukyukyukyuk)
+            </p>
+        </div>
+    )
+   
+}
+export default About ;
